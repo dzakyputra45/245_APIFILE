479562342478b2790c5fc6c9c260a5b034e0911a
dzakyputra45 <dzakypratama45@gmail.com>
2025-11-25 11:04:36 +0700

    Menambahkan file Api.js dan tambahkan di index,js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const ConnectionDatabase = require('./config/db');
+const router = require('./routes/api');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1', apirouter);
 
 async function startServer() {
     await ConnectionDatabase();
